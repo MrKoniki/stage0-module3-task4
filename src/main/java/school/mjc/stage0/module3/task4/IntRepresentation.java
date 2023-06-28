57a21eb475832880abd2968ea2230d03a25e935b
nikitakorneichuk <korneichuk.nikita@gmail.com>
2023-06-29 02:04:27 +0300

    Add new code

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int cInt = 99;
+        int hInt = 104;
+        int aInt = 97;
+        int rInt = 114;
+        char cChar = (char)cInt;
+        char hChar = (char)hInt;
+        char aChar = (char)aInt;
+        char rChar = (char)rInt;
+        String output = "" + cChar + hChar + aChar + rChar;
+        System.out.println(output);
     }
 }
